8c63ef0969322d5d5b1c4819d4a126a7cfe10391
Azure Zanculmarktum <zanculmarktum@gmail.com>
2020-08-14 07:48:34 +0700

    add android-udev-rules

---
+++ android-udev-rules/default.nix
@@ -0,0 +1,13 @@
+self: super:
+
+with builtins;
+
+{
+  ${baseNameOf ./.} = (super.callPackage <nixpkgs/pkgs/os-specific/linux/android-udev-rules> { }).overrideAttrs (oldAttrs: {
+    installPhase = oldAttrs.installPhase + ''
+      sed -i \
+        -e 's/ATTR{idVendor}=="2b4c", ENV{adb_user}="yes"/&\n\n# Vivo\nATTR{idVendor}=="2d95", ENV{adb_user}="yes"/' \
+        $out/lib/udev/rules.d/51-android.rules
+    '';
+  });
+}
