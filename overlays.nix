b799fe2014a321fd4e546a45f6e68fab0c718be2
Azure Zanculmarktum <zanculmarktum@gmail.com>
2020-09-23 17:56:09 +0700

    don't clutter up the namespaces

--- overlays.nix
+++ overlays.nix
@@ -2,17 +2,14 @@
   ( #self: super:
     final: prev:
 
-    with import ./modules/compilers.nix final prev;
-
     {
       android-udev-rules = (prev.callPackage (prev.path + "/pkgs/os-specific/linux/android-udev-rules") { }).overrideAttrs (oldAttrs: {
         installPhase = oldAttrs.installPhase + ''
-      sed -i \
-        -e 's/ATTR{idVendor}=="2b4c", ENV{adb_user}="yes"/&\n\n# Vivo\nATTR{idVendor}=="2d95", ENV{adb_user}="yes"/' \
-        $out/lib/udev/rules.d/51-android.rules
-    '';
+          sed -i \
+            -e 's/ATTR{idVendor}=="2b4c", ENV{adb_user}="yes"/&\n\n# Vivo\nATTR{idVendor}=="2d95", ENV{adb_user}="yes"/' \
+            $out/lib/udev/rules.d/51-android.rules
+        '';
       });
-
 
       conky-nox = prev.callPackage (prev.path + "/pkgs/os-specific/linux/conky") {
         x11Support = false;
@@ -37,20 +34,20 @@
         inherit patches;
       }).overrideAttrs (oldAttrs: {
         postPatch = ''
-      substituteInPlace dwm.c \
-        --replace '@psmisc@' '${final.pkgs.psmisc}/bin/'
-      substituteInPlace config.def.h \
-        --replace '@dmenu@' '${final.pkgs.dmenu}/bin/' \
-        --replace '@j4_dmenu_desktop@' '${final.pkgs.j4-dmenu-desktop}/bin/' \
-        --replace '@alacritty@' '${final.pkgs.alacritty}/bin/'
-    '';
+          substituteInPlace dwm.c \
+            --replace '@psmisc@' '${final.pkgs.psmisc}/bin/'
+          substituteInPlace config.def.h \
+            --replace '@dmenu@' '${final.pkgs.dmenu}/bin/' \
+            --replace '@j4_dmenu_desktop@' '${final.pkgs.j4-dmenu-desktop}/bin/' \
+            --replace '@alacritty@' '${final.pkgs.alacritty}/bin/'
+        '';
       });
 
-      inherit gnat gnat6 gnat9 gnatboot;
+      inherit (import ./modules/compilers.nix final prev) gnat gnat6 gnat9 gnatboot;
 
       nixpkgs-manual = prev.callPackage (prev.path + "/doc") { };
 
-      ungoogled-chromium = prev.callPackage ./ungoogled-chromium/package (prev.config.chromium or {});
+      ungoogled-chromium = prev.callPackage ./ungoogled-chromium (prev.config.chromium or {});
 
       zathura = prev.callPackage (prev.path + "/pkgs/applications/misc/zathura") {
         useMupdf = true;
